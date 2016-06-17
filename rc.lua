7bbb9c938cdc7b5e37063deb1c43a8afa0f21674
xiaochengxin <xiaochengxin@imcoming.cn>
2016-06-17 14:07:55 +0800

    dev

--- rc.lua
+++ rc.lua
@@ -510,10 +510,10 @@ root.keys(globalkeys)
 awful.rules.rules = {
     -- All clients will match this rule.
     { rule = { },
-    -- focus = myfocus_filter,
-    focus = awful.client.focus.filter,
     properties = { border_width = beautiful.border_width,
     border_color = beautiful.border_normal,
+    -- focus = myfocus_filter,
+    focus = awful.client.focus.filter,
     raise = true,
     keys = clientkeys,
     buttons = clientbuttons } },
@@ -523,7 +523,7 @@ awful.rules.rules = {
     -- properties = { floating = true } },
     -- { rule = { class = "gimp" },
     -- properties = { floating = true } },
-    -- -- Set Firefox to always map on tags number 2 of screen 1.
+    -- Set Firefox to always map on tags number 2 of screen 1.
     -- { rule = { class = "Firefox" },
     -- properties = { tag = tags[1][2] } },
     { rule = { class = "electronic-wechat" },
